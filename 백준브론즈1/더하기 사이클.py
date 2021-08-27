72b0d76c4ae745d0d2142b7ebd6c6279558030f5
jsggo2001 <jsggo2001@naver.com>
2021-08-27 13:59:43 +0900

    Update 더하기 사이클.py

--- 백준브론즈1/더하기 사이클.py
+++ 백준브론즈1/더하기 사이클.py
@@ -3,13 +3,12 @@ n = a
 count = 0
 while True:
     if len(str(n)) == 1:
-        n = ((n%10)*10) + n
+        n = ((n % 10) * 10) + n
         count += 1
     elif len(str(n)) != 1:
-        n = ((n%10)*10) + (((n//10) + (n%10))%10)
+        n = ((n % 10) * 10) + (((n // 10) + (n % 10)) % 10)
         count += 1
     if n == a:
         break
 
 print(count)
-
