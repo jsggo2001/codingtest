52bbb84b2cdd42593ff2bcd660548df4a803569c
jsggo2001 <jsggo2001@naver.com>
2021-09-09 13:42:37 +0900

    update

--- 재귀함수.py
+++ 재귀함수.py
@@ -2,8 +2,8 @@
 
 def factorial_iterative(n):
     result = 1
-
     for i in range(1, n+1):
+        print(result)
         result *= i
     return result
 
@@ -12,8 +12,8 @@ def factorial_recursive(n):
         return 1
     return n*factorial_recursive(n-1)
 
-print('반복적:', factorial_iterative(5))
-print('재귀적:', factorial_recursive(5))
+print('반복적:', factorial_iterative(1000000))
+print('재귀적:', factorial_recursive(1000000))
 
 
 
