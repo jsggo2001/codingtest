2d2d89452f5cf41b7881cfd648c4fa94e0e2b914
jsggo2001 <jsggo2001@naver.com>
2021-09-22 19:17:04 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-a = [1,2,3,4,5,6,7,8,9,10]
+a = [1,2,3,4,1,6,7,8,9,10]
 
-start = 0
-end = len(a) - 1
-target = 10
+del a[0]
 
-while True:
-    mid = (start + end) // 2
-
-    if a[mid] == target:
-        print(mid)
-        break
-    elif a[mid] > target:
-        end = mid - 1
-    else:
-        start = mid + 1
-
-
+print(a)
 
