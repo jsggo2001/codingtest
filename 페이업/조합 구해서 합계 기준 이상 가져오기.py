08adeaf2bd34a09838fe1927e6ac7fb96b3ecd06
jsggo2001 <jsggo2001@naver.com>
2021-09-30 11:02:15 +0900

    Create 조합 구해서 합계 기준 이상 가져오기.py

---
+++ 페이업/조합 구해서 합계 기준 이상 가져오기.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+
+def solution(arr, k, t):
+    answer = 0
+    lst = []
+    for i in range(k,len(arr)+1):
+        lst.extend(list(combinations(arr, i)))
+    for i in lst:
+        if sum(i) <= t:
+            answer += 1
+
+    return answer
+
+print(solution([1,1,2,2],2,3))
