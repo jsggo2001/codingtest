00701198d20b9ab64eb60abc5e4e1db0e4d77c42
jsggo2001 <jsggo2001@naver.com>
2021-10-11 23:18:57 +0900

    Update 개선된 서로소 집합 알고리즘.py

--- 그래프 이론/개선된 서로소 집합 알고리즘.py
+++ 그래프 이론/개선된 서로소 집합 알고리즘.py
@@ -5,7 +5,6 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
